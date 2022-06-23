57f7829cb44be86e0ae81e6ad2bd6e09f082248c
boonaki <joshdurr17@gmail.com>
2022-06-22 23:08:23 -0400

    added fetch to server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
-const PORT = 8000
-// const fetch = require('node-fetch')
+const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+
 require('dotenv').config()
+const PORT = 8000
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
+// app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
+    // res.sendFile(__dirname + '/index.html')
+    const url = 'https://tea-api-boonaki.herokuapp.com/api/teas'
+    fetch(url)
+        .then(res => res.json()) //parse response as json
+        .then(data => {
+            //TODO: convert data into array to be accesed by ejs
+            res.render('index.ejs', { teainfo : data.map})
+        })
+        .catch(err => {
+            console.error(err)
+        })
 })
 
 app.listen(PORT, () => {
