a21e28f514f44b3e5005608aa5ff9c1e354db08f
boonaki <joshdurr17@gmail.com>
2022-07-07 13:16:33 +0200

    changed port for heroku compat

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
 
 require('dotenv').config()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
