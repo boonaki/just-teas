4bf8e8e9d1a32bf622bbc75b500b2b0b877ffd00
boonaki <joshdurr17@gmail.com>
2022-08-21 17:09:12 -0400

    removed unused code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,8 @@ function findTeaDesc(id){
     let expandeds = document.getElementsByClassName('basictea')
     for(let i = 0; i < expandeds.length ;i++){
         expandeds[i].classList.add('hidden')
+        // expandeds[i].classList.remove('current')
     }
     document.getElementById(`tea-${id}`).classList.remove('hidden')
+    // document.getElementById(`tea-${id}`).classList.add('current')
 }
